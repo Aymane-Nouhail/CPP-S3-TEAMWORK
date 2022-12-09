a4c9807a78f3f42b16ed97c7a3cc3630db4db18f
AlaeChoukr <91984437+AlaeChoukr@users.noreply.github.com>
2022-12-09 16:59:03 +0100

    Update TP04.cpp

--- TP04.cpp
+++ TP04.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
 #include <list>
-
-class Directory(file){
+using namespace std ; 
+class Directory(File){
     private : 
-        std::list <Directory> sub_directories = {} ; 
-        std::list <TextFile> text_files = {} ; 
-        std::list <ExecFile> exec_files = {} ; 
-        directory* parent_directory ; 
+        list <Directory> sub_directories = {} ; 
+        list <TextFile> text_files = {} ; 
+        list <ExecFile> exec_files = {} ; 
+        Directory* parent_directory ; 
     public : 
-       void set_sub_directories(std::list<Directory> my_sub_directory) {sub_directories = my_sub_directory}
-       std::list<Directory> get_sub_directories() {return sub_directories}
-       void set_text_files(std::list<TextFile> my_text_file) {text_files = my_text_file}
-       std::list<TextFile> get_text_files() {return text_files}
-       void set_exec_files(std::list<ExecFile> my_exec_file) {exec_files = my_exec_file}
-       std::list<Exec_files> get_exec_files() {return exec_files}
+       Directory(list <Directory> x, list <TextFile> y , list <ExecFile> z){
+        this->x = sub_directories ;
+        this->y = text_files ; 
+        this->z = exec_files;
+       }
+       void set_sub_directories(list<Directory> my_sub_directory) {sub_directories = my_sub_directory}
+       list<Directory> get_sub_directories() {return sub_directories ;}
+       void set_text_files(list<TextFile> my_text_file) {text_files = my_text_file}
+       list<TextFile> get_text_files() {return text_files ;}
+       void set_exec_files(list<ExecFile> my_exec_file) {exec_files = my_exec_file}
+       list<Exec_files> get_exec_files() {return exec_files ;}    
 };
 
-
+([])
 
 
 
 
 class TextFile(File){
     private : 
-    std::string location ; 
+    string location ; 
     public : 
-    void set_location(std::string my_location){location = my_location ;}
+    void set_location(string my_location){location = my_location ;}
     std:string get_location(){return location ; }
 
 };
