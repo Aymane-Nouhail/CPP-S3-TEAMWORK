dff61c3a445bbc76f534689b36f748def10f214d
Aymane Nouhail <92037663+Aymane-Nouhail@users.noreply.github.com>
2022-12-09 15:15:34 +0100

    Create tp4.cpp

---
+++ tp4.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+struct date{
+    int day;
+    int month;
+    int year;
+};
+
+
+class File{
+    public:
+        void set_name(string my_name){name = my_name;}
+        string get_name(){return name;}
+        void set_permission(int my_permission){
+
+            if my_permission != 'r'
+            permission = my_permission;}
+        int get_permission(){return permission;}
+        void set_date(my_day,my_month,my_year){date = {my_day,my_month,my_year};}
+        date get_date(){return date_creation;}
+    private:
+        string name;
+        int permission;
+        date date_creation;
+};
+
+class ExecFile(TextFile){
+    private:
+        bool flag;
+        ExecFile 
+};
+
+class Shell{
+    private:
+        Directory ROOTDIR
+};
